486287e78c2ff81cbccd96da2937455ec460349d
Sharawelly <lordmarawanshrief@gmail.com>
2025-02-28 09:48:15 +0200

    formating files

--- lib/detailedScreen/detailed_screen.dart
+++ lib/detailedScreen/detailed_screen.dart
@@ -1,12 +1,12 @@
-import 'package:coffee_app/buy_coffee.dart';
-import 'package:coffee_app/coffee_main_details.dart';
-import 'package:coffee_app/description.dart';
+import 'package:coffee_app/detailedScreen/buy_coffee.dart';
+import 'package:coffee_app/detailedScreen/coffee_main_details.dart';
+import 'package:coffee_app/detailedScreen/description.dart';
 import 'package:coffee_app/model/coffee_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'coffee_size.dart';
-import 'data/coffe_items_data.dart';
+import '../data/coffe_items_data.dart';
 
 class DetailedScreen extends StatelessWidget{
   const DetailedScreen({required this.coffeeItem, super.key});
